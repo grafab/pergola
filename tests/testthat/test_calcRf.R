41fce58526fa591f7add9bf5d23a6cdf3e4185b0
FABIAN GRANDKE <grafabian@gmail.com>
2016-07-14 22:34:13 +0200

    simplify test design

--- tests/testthat/test_calcRf.R
+++ tests/testthat/test_calcRf.R
@@ -17,10 +17,8 @@ test_that("calcRec returns values from 0 to 0.5", {
   mat <- calcRec(matrix(sample(
     0:4, size = 8000, replace = TRUE
   ), ncol = 800), ploidy = 4)
-  expect_true(min(mat) <= 0.5)
   expect_true(min(mat) >= 0)
   expect_true(max(mat) <= 0.5)
-  expect_true(max(mat) >= 0.5)
 })
 
 test_that("calcRec returns only zeros on diagonal", {
